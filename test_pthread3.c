3baad2ccab059cc103a4beff92638362a5a69ea6
Fera95 <ferarios95@gmail.com>
2020-07-05 22:15:28 -0600

    Se prueban multiple creadores

--- test_pthread3.c
+++ test_pthread3.c
@@ -9,37 +9,115 @@
 
 #endif
 
-#define NTHREADS 10
+#define NTHREADS 9
 void *thread_function(void *);
 lpthread_mutex_t mutex1;
 int  counter = 0;
 
-main()
+struct args {
+   lpthread_t idB;
+   int numero;
+};
+
+void *thread_function(void *numero)
+{  
+   int a = *((int *) numero);
+   printf("Numero %d\n", a);
+   free(numero);
+}
+
+
+void *crearhilo1(void *input){
+ 
+ lpthread_t x = ((struct args*)input)->idB ;
+ int num = ((struct args*)input)->numero;
+
+ int *arg = malloc(sizeof(*arg));
+        if ( arg == NULL ) {
+            fprintf(stderr, "Couldn't allocate memory for thread arg.\n");
+            exit(EXIT_FAILURE);
+        }
+
+        *arg = num;
+ printf("Creador1 \n");
+ lpthread_create( &x, NULL, thread_function, arg);
+}
+
+
+void *crearhilo2(void *input){
+ 
+ lpthread_t x = ((struct args*)input)->idB ;
+ int num = ((struct args*)input)->numero;
+
+ int *arg = malloc(sizeof(*arg));
+        if ( arg == NULL ) {
+            fprintf(stderr, "Couldn't allocate memory for thread arg.\n");
+            exit(EXIT_FAILURE);
+        }
+
+        *arg = num;
+  printf("Creador2 \n");
+ lpthread_create( &x, NULL, thread_function, arg);
+}
+
+
+
+void *crearhilo3(void *input){
+ 
+ lpthread_t x = ((struct args*)input)->idB ;
+ int num = ((struct args*)input)->numero;
+
+ int *arg = malloc(sizeof(*arg));
+        if ( arg == NULL ) {
+            fprintf(stderr, "Couldn't allocate memory for thread arg.\n");
+            exit(EXIT_FAILURE);
+        }
+
+        *arg = num;
+  printf("Creador3 \n");
+ lpthread_create( &x, NULL, thread_function, arg);
+}
+
+
+
+int main()
 {
-   lpthread_t thread_id[NTHREADS];
+   
    int i, j;
+
+   lpthread_t thread_id[NTHREADS];
+   lpthread_t thread_id2[NTHREADS];
+
 
    for(i=0; i < NTHREADS; i++)
    {
-      lpthread_create( &thread_id[i], NULL, thread_function, NULL );
+
+       struct args *datos = (struct args *)malloc(sizeof(struct args));
+   
+   
+         datos->idB =  thread_id2[i];
+       datos->numero = i;
+      
+
+      
+
+         if(i<=3){
+            lpthread_create( &thread_id[i], NULL, crearhilo1, (void *)datos);
+         }
+         if(i>3 && i<=6){
+            lpthread_create( &thread_id[i], NULL, crearhilo2, (void *)datos);
+         }
+
+         if(i>6){
+            lpthread_create( &thread_id[i], NULL, crearhilo3, (void *)datos);
+         }
+         
+      
    }
 
-   for(j=0; j < NTHREADS; j++)
-   {
-      lpthread_join( thread_id[j], NULL); 
-   }
   
-   /* Now that all threads are complete I can print the final result.     */
-   /* Without the join I could be printing a value before all the threads */
-   /* have been completed.                                                */
+  
+                                    
 
-   printf("Final counter value: %d\n", counter);
 }
 
-void *thread_function(void *dummyPtr)
-{
-   printf("Thread number %d\n", lpthread_self());
-   lpthread_mutex_lock( &mutex1 );
-   counter++;
-   lpthread_mutex_unlock( &mutex1 );
-}
